846ef3c4dd3dc395bfd79f4e7f69d250a9626a0b
duyuqian <15738381799@163.com>
2020-01-04 19:20:04 +0800

    [duyuqian] feat: complete user practice

--- users/ajax.js
+++ users/ajax.js
@@ -8,6 +8,36 @@ options = {
   fail: function(error) {}    // 请求失败或出错后调用此方法
 }
 **/
-window.ajax = function (options) {
+window.ajax = function(options) {
+    var option = {
+        url: options.url || '',
+        method: options.method.toLocaleUpperCase() || 'GET',
+        headers: options.headers || '',
+        data: options.data || null,
+        onSuccess: options.success || function(result) {},
+        onFail: options.fail || function(error) {}
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(option.method, option.url, true);
+    if (option.method === 'POST' || option.method === 'PUT') {
+        //xhr.setRequestHeader(option.headers.key, option.headers.value);
+        xhr.setRequestHeader('content-type', 'application/json');
+        option.data = JSON.stringify(option.data);
+    }
+
+    xhr.onerror = () => option.onFail(xhr.status);
+    xhr.onload = () => option.onSuccess(JSON.parse(xhr.responseText));
+
+    // xhr.onreadystatechange = function() {
+    //     if (xhrObject.readyState === 4) {
+    //         if (xhrObject.status === 200 || (xhr.status === 201 && option.method === 'POST')) {
+    //             option.onSuccess(JSON.parse(xhr.responseText));
+    //         } else {
+    //             option.onFail(xhr.status);
+    //         }
+    //     }
+    // };
+    xhr.send(option.data);
 
 }
--- users/index.js
+++ users/index.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     const API_ROOT = "http://localhost:3000/users";
     let $userList = document.getElementById('user-list');
     let $username = document.getElementById('username');
@@ -6,26 +6,71 @@
 
     // TODO： get user list，when success, run renderUserList
     function getListData() {
-
+        let options = {
+            url: API_ROOT,
+            method: "GET",
+            success: function(res) {
+                renderUserList(res);
+            }, // 请求成功后调用此方法
+            fail: function(error) {
+                    console.log('ERROR');
+                } // 请求失败或出错后调用此方法
+        };
+        ajax(options);
     }
 
     // TODO:  add user info,when success, run addItem
     function addItemData(data) {
-
+        let options = {
+            url: API_ROOT,
+            method: "POST",
+            data: data,
+            success: function(data) {
+                addItem(data);
+            }, // 请求成功后调用此方法
+            fail: function(error) {
+                    console.log('ERROR');
+                } // 请求失败或出错后调用此方法
+        };
+        ajax(options);
     }
 
     // TODO: update user info,when success, run updateItem
     // 提示：Math.random().toString(36).substring(2) 生成随机的字符串
     function updateItemData(id) {
-
+        let options = {
+            url: API_ROOT + '/' + id,
+            method: "PUT",
+            data: {
+                username: Math.random().toString(36).substring(2),
+                password: Math.random().toString(36).substring(2)
+            },
+            success: function(data) {
+                updateItem(data);
+            }, // 请求成功后调用此方法
+            fail: function(error) {
+                    console.log('ERROR');
+                } // 请求失败或出错后调用此方法
+        };
+        ajax(options);
     }
 
     // TODO: delete user info,,when success, run  deleteItem
     function deleteItemData(id) {
-
+        let options = {
+            url: API_ROOT + '/' + id,
+            method: "DELETE",
+            success: function(res) {
+                deleteItem(id);
+            }, // 请求成功后调用此方法
+            fail: function(error) {
+                    console.log('ERROR');
+                } // 请求失败或出错后调用此方法
+        };
+        ajax(options);
     }
 
-    document.getElementById('add-btn').addEventListener('click', function () {
+    document.getElementById('add-btn').addEventListener('click', function() {
         let username = document.getElementById('username').value;
         let password = document.getElementById('password').value;
         let data = {
@@ -35,7 +80,7 @@
         addItemData(data);
     })
 
-    $userList.addEventListener('click', function (event) {
+    $userList.addEventListener('click', function(event) {
         let id = event.target.getAttribute('data-id') || event.target.parentElement.getAttribute('data-id');
 
         if (!id) {
